7f006fb6f85920fb41c0db90d883866f943558ab
sodexkings <71529813+sodexkings@users.noreply.github.com>
2024-04-30 18:48:45 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function Adduser() {
+    player1_name = document.getElementById("player1_name_input").value;
+    player2_name = document.getElementById("player2_name_input").value;
+  
+      localStorage.setItem("player1_name", player1_name);
+      localStorage.setItem("player2_name", player2_name);
+  
+      window.location = "game_page.html";
+  }
+
+  function send(){
+    number1 = document.getElementById("number1").value;
+    number2 = document.getElementById("number2").value
+    actual_answer = parseInt(number1) * parseInt(number2)
+    question_numbers = "<h4>" + number1 + "X"+ number2 +"</h4>";
+  input_box = "<br>Answer : <input type='text' id= 'input_check_box'>";
+  row = question_numbers + input_box + check_button ;
+  document.getElementById("output").innerHTML = row;
+  document.getElementById("number1").value = "";
+  document.getElementById("number2").value = "";
+  }
+  
+  
